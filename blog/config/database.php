53788fa0d732a84e243b13f1280d4868b7f83cfc
fentonhaslam <fentonhaslam@gmail.com>
2019-10-17 16:06:45 +0100

    amending database

--- blog/config/database.php
+++ blog/config/database.php
@@ -58,6 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'port' => env('DB_PORT', 3306),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
@@ -76,6 +77,7 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+            'port' => env('DB_PORT', 3306),
         ],
 
         'sqlsrv' => [
